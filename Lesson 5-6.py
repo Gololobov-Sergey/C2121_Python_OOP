4d2372db33a8285edcfdd526f052fc300504e508
gololobov <gololobov.serhiy@gmail.com>
2024-09-21 11:03:40 +0300

    21.09.2024 #2

--- Lesson 5-6.py
+++ Lesson 5-6.py
@@ -1,10 +1,51 @@
+import random
+
+
 class Human:
     def __init__(self, name, job=None, car=None):
         self.name = name
         self.house = House()
         self.car = car
         self.job = job
+        self.money = 100
 
+    def eat(self):
+        eat = random.randint(1, 10)
+        print(f"Я з'їв {eat}% їжи")
+        self.house.food -= eat
+
+    def chill(self):
+        print(f"Я відпочиваю")
+        self.house.pollution += 2
+        self.money -= 5
+
+    def shopping(self):
+        self.money -= random.randint(1,10)
+        if not self.car.drive(random.randint(1,10)*10):
+            print("Пішли в магазин пішки")
+        self.house.food += random.randint(1,10)
+
+    def cleaning(self):
+        print("Я прибираю в будинку")
+        c = random.randint(0, 1)
+        if c == 1:
+            print("Генеральне прибирання")
+            self.house.pollution = 0
+        else:
+            print("Витерли пилюку")
+            self.house.pollution -= 5
+        if self.house.pollution < 0:
+            self.house.pollution = 0
+
+    def work(self):
+        self.money += 10
+        if self.car != None:
+            if not self.car.drive(20):
+                print("Я поїхав на роботу")
+            else:
+                print("Я сходив на роботу")
+        else:
+            print("Я сходив на роботу")
 
 class Car:
     def __init__(self, model):
@@ -24,7 +65,7 @@ class Car:
     def drive(self, length):
         delta_fuel = length * 0.1
         if self.fuel - delta_fuel < 0:
-            print("Подорож не можлива, не вистачає пального")
+            print("Подорож на авто не можлива, не вистачає пального")
             return False
         else:
             self.fuel -= delta_fuel
@@ -64,10 +105,12 @@ class House:
 
 
 
-human1 = Human("Serg", Job("Викладач", 1000))
-human2 = Human("Anna")
+human1 = Human("Serg", job=Job("Викладач", 1000), car=Car("BMW X11"))
+human1.work()
 
-car = Car("BMW X11")
-car.add_passenger(human1, human2)
-
-car.print_passenger()
+# human2 = Human("Anna")
+#
+# car = Car("BMW X11")
+# car.add_passenger(human1, human2)
+#
+# car.print_passenger()
